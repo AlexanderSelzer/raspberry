4e589042aa5805d6db07a01ff984eb3754e3ccd4
Alexander Selzer <aselzer3@gmail.com>
2014-08-05 12:29:29 +0200

    fixed streaming quality & CPU usage

--- mjpg-stream.sh
+++ mjpg-stream.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mjpg_streamer -i "/usr/local/lib/input_uvc.so -d /dev/video0 -q 94 -f 24 -r 1280x720" \
+mjpg_streamer -i "/usr/local/lib/input_uvc.so -f 24 -r 1280x720 -d /dev/video0" \
 -o "/usr/local/lib/output_http.so -p 8080 -w ~/mjpg-streamer"
